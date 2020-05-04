d6397dc85d20166ec6544ae21aa4182f0f96570e
Гудым Артём <sabbirbeb@mail.ru>
2020-05-04 21:13:10 +0300

    Updates

---
+++ precache-manifest.6259235352b78cac493c2f013a10b9a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1023a1c351f851d8d89bee294790988d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b685e745b6795655918b",
+    "url": "/static/css/2.34bdae44.chunk.css"
+  },
+  {
+    "revision": "8e7cde30c3e5ec6c5ed5",
+    "url": "/static/css/main.aa0dc05f.chunk.css"
+  },
+  {
+    "revision": "b685e745b6795655918b",
+    "url": "/static/js/2.281cdc9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.281cdc9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7cde30c3e5ec6c5ed5",
+    "url": "/static/js/main.1e8ef994.chunk.js"
+  },
+  {
+    "revision": "e40e1b706e39b2a5a008",
+    "url": "/static/js/runtime-main.9b809c99.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
